2a83c9e9129e4f33bd946f836b00e3a45e9a0a86
holk <yellowmac@sina.com>
2020-08-14 00:00:17 +0800

    finish

--- src/App.vue
+++ src/App.vue
@@ -8,8 +8,36 @@
                     <v-col cols="10">
 
                     </v-col>
+
                     <v-col style="text-align: right">
-                        <v-icon class="mr-2">mdi-lightbulb-on-outline</v-icon>
+                        <transition>
+                            <div>
+                                <v-tooltip bottom>
+                                    <template v-slot:activator="{ on, attrs }">
+                                        <v-icon class="mr-1 mb-1" v-bind="attrs"
+                                                v-on="on">mdi-lightbulb-on-outline
+                                        </v-icon>
+                                    </template>
+                                    <span>{{ hint }}</span>
+                                </v-tooltip>
+                                |
+                                <v-icon class="mr-1 mb-1" @click="overlay = !overlay">mdi-help-box</v-icon>
+                                <v-overlay :value="overlay" style="text-align: center">
+                                    <v-card light elevation="6">
+                                        <v-card-title>欢迎你。<br>这是一个解谜网站<br>你需要通过各种方法，根据题目找到答案。
+                                        <br>它会涉及到各个领域的知识<br>找到正确答案，输入后按下回车即可进入下一关
+                                            <br>打开脑洞，祝你好运！</v-card-title>
+                                    </v-card>
+                                    <v-btn icon @click="overlay = false" class="mt-10">
+                                        <v-icon>mdi-close</v-icon>
+                                    </v-btn>
+                                </v-overlay>
+                                |
+                                <a href="https://holk.tech" style="text-decoration: none">
+                                    <v-icon class="mr-1">mdi-message-text-outline</v-icon>
+                                </a>
+                            </div>
+                        </transition>
                     </v-col>
                 </v-row>
             </v-container>
@@ -21,16 +49,14 @@
 
                         <transition>
                             <v-card style="top: 12rem;background: none;text-align: center" flat v-if="show">
-                                <div style="font-size: 3rem" class="grey--text text--darken-3">
-                                    <strong>欢迎来解谜</strong><br>
-                                    <span style="font-size: 1.8rem">根据题目找到答案以后回车即可<br>祝你好运</span>
+                                <div style="font-size: 3rem" class="grey--text text--darken-3" v-html="msg">
                                 </div>
                             </v-card>
                         </transition>
                         <v-card style="top: 17rem;background: none" flat>
                             <transition>
                                 <v-text-field
-                                        label="来输入答案吧"
+                                        label="输入答案"
                                         solo
                                         v-if="showtxt"
                                         prepend-inner-icon="mdi-arrow-right"
@@ -39,13 +65,15 @@
                                         :success="suc"
                                         @change="change"
                                         v-model="val"
+                                        id="input"
+                                        :messages="zt"
                                 ></v-text-field>
                             </transition>
                         </v-card>
                         <transition>
-                        <v-card flat style="top: 17rem;background: none;text-align: right"
-                                class="grey--text mr-1" v-if="show">第{{ id }}关
-                        </v-card>
+                            <v-card flat style="top: 17rem;background: none;text-align: right"
+                                    class="grey--text mr-1" v-if="show">第{{ level }}关
+                            </v-card>
                         </transition>
                     </v-col>
                     <v-col>
@@ -65,29 +93,71 @@
         components: {},
 
         data: () => ({
-            id: 1,
+            level: 1,
             show: false,
             showtxt: false,
             err: false,
             suc: false,
             val: '',
+            msg: '<strong>欢迎你</strong><br><span style="font-size: 1.8rem">根据题目找到答案以后回车即可' +
+                '<br>祝你好运</span>',
+            zt: '',
+            hint: '看看文本框里写的什么？',
+            overlay: false
         }),
         mounted() {
             this.show = true
             this.showtxt = true
+            Vue.axios.get('https://holk.tech/riddle/visitors.php')
         },
-        methods:{
-            change(){
-                Vue.axios.get('https://holk.tech/',{
-                    params:{
-                        key: this.val
+        methods: {
+            change() {
+                Vue.axios.get('https://holk.tech/riddle/vaildkey.php', {
+                    params: {
+                        key: this.val,
+                        level: this.level
                     }
                 }).then((res) => {
-                    if (res.data === 'true'){
-                        this.show = false
-
+                    if (res.data === true) {
+                        this.nextLevel()
+                    } else {
+                        this.err = true
+                        this.zt = '答案错误'
                     }
+                }).catch((err) => {
+                    alert(err)
                 })
+            },
+            nextLevel() {
+                Vue.axios.get('https://holk.tech/riddle/gethint.php', {
+                    params: {
+                        level: parseInt((this.level).toString()) + 1
+                    }
+                }).then((res) => {
+                    this.level = res.data['level']
+                    this.hint = res.data['hint']
+                    this.tran(res.data['question'], res.data['id'])
+                    setTimeout(function () {
+                        document.getElementById('input').blur()
+                    }, 1010)
+                }).catch((err) => {
+                    alert(err)
+                })
+            },
+            tran(msg, level) {
+                this.show = false
+                this.msg = msg
+                this.level = level
+                this.suc = true
+                this.zt = ''
+                setTimeout(this.showed, 1000)
+            },
+            showed() {
+                this.show = true
+                this.suc = false
+                this.zt = true
+                this.err = false
+                this.val = ''
             }
         }
     };
@@ -102,9 +172,8 @@
     .v-enter-active {
         transition: all 2.5s ease;
     }
+
     .v-leave-active {
         transition: all 1s ease;
     }
-
-
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,27 @@
+const CompressionPlugin = require("compression-webpack-plugin")
+
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+  runtimeCompiler: true,
+  publicPath: './',  //输出的根路径
+  productionSourceMap: false,
+  devServer: {
+    port: 8888 //端口
+  },
+  configureWebpack: () => {
+    if (process.env.NODE_ENV === 'production') {
+      return {
+        plugins: [
+          new CompressionPlugin({
+            test: /\.js$|\.html$|\.css$|\.jpg$|\.jpeg$|\.png/, // 需要压缩的文件类型
+            threshold: 10240, // 归档需要进行压缩的文件大小最小值，我这个是10K以上的进行压缩
+            deleteOriginalAssets: false, // 是否删除原文件
+            minRatio: 0.8
+          })
+        ]
+      }
+    }
+  }
 }
