3b45583fb864000c9094f53675dd1aba7be1112e
holk <yellowmac@sina.com>
2020-08-13 10:45:04 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -19,23 +19,28 @@
                     </v-col>
                     <v-col cols="10">
 
-                        <v-expand-transition>
-                            <v-card style="top: 12rem;background: none;text-align: center" flat>
+                        <transition>
+                            <v-card style="top: 12rem;background: none;text-align: center" flat v-if="show">
                                 <div style="font-size: 3rem" class="grey--text text--darken-3">
                                     <strong>欢迎来解谜</strong><br>
                                     <span style="font-size: 1.8rem">根据题目找到答案以后回车即可<br>祝你好运</span>
                                 </div>
                             </v-card>
-                        </v-expand-transition>
+                        </transition>
                         <v-card style="top: 17rem;background: none" flat>
-                            <v-text-field
-                                    label="来输入答案吧"
-                                    solo
-                            ></v-text-field>
+                            <transition>
+                                <v-text-field
+                                        label="来输入答案吧"
+                                        solo
+                                        v-if="showtxt"
+                                ></v-text-field>
+                            </transition>
                         </v-card>
+                        <transition>
                         <v-card flat style="top: 17rem;background: none;text-align: right"
-                                class="grey--text mr-1">第{{ id }}关
+                                class="grey--text mr-1" v-if="show">第{{ id }}关
                         </v-card>
+                        </transition>
                     </v-col>
                     <v-col>
                     </v-col>
@@ -53,7 +58,28 @@
         components: {},
 
         data: () => ({
-            id:1
+            id: 1,
+            show: false,
+            showtxt: false
         }),
+        mounted() {
+            this.show = true
+            this.showtxt = true
+        }
     };
 </script>
+
+<style>
+    .v-enter,
+    .v-leave-to {
+        opacity: 0;
+        transform: translateX(100px);
+    }
+
+    .v-enter-active,
+    .v-leave-active {
+        transition: all 2.5s ease;
+    }
+
+
+</style>
